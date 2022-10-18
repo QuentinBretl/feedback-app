7e4e3cfb2356be82fe43561ac38c9653e3c20b0a
QuentinBretl <quentinbretelle@orange.fr>
2022-10-18 02:35:46 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import FeedbackList from './components/FeedbackList.jsx';
+import FeedbackStats from './components/FeedbackStats.jsx';
+import FeedbackForm from './components/FeedbackForm.jsx';
+import AboutIconLink from './components/AboutIconLink.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import { FeedbackProvider } from './context/FeedbackContext.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <div className='container'>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                  <AboutIconLink />
+                </div>
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackList() {
+
+  const {feedback} = useContext(FeedbackContext)
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback yet...</p>
+    }
+
+    return (
+      <div className='feedback-list'>
+        {feedback.map((item) => (
+            <FeedbackItem key={item.id} item={item} />
+        ))}
+    </div>)
+
+  /*return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>))}
+    </div>
+  ) */
+}
+
+export default FeedbackList
